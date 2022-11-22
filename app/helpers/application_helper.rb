1315337c92f75140fab7ff811c525e63dc5d65ac
MG75 <maj.gabersek@gmail.com>
2022-11-22 09:38:02 +0100

    add active class to current link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
     def display_alert
         content_tag(:p, alert, calss: "alert") if alert.present?
     end
+
+    def display_right_navbar_options
+        if user_signed_in? 
+            render 'dropdown'
+        else
+            render 'login_buttons'
+        end
+    end
+
+    def display_active_class(link_page)
+        "active" if current_page?(link_page)
+    end
 end
