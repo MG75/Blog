7d246f014dc50f7c95375018df0d2265062a59fc
MG75 <maj.gabersek@gmail.com>
2022-12-13 09:15:22 +0100

    add comments resource

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
     validates_presence_of :title, :body
+    has_many :comments, dependent: :destroy
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments only: [:update, :destroy, :create]
   devise_for :users
-  resources :posts
+  resources :posts 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
